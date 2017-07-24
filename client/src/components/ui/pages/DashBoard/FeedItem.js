9085d7f0d9c822ae2c539b4232732eae042dc84a
Peter Wang <happypeter1983@gmail.com>
2017-07-25 07:46:04 +0800

    toggleExpand

--- client/src/components/ui/pages/DashBoard/FeedItem.js
+++ client/src/components/ui/pages/DashBoard/FeedItem.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
+import {
+  Link
+} from 'react-router-dom'
+
 class FeedItem extends Component {
 
   state = {
     expand: false
   }
-  toggleExpand = () => {
+  toggleExpand = (option) => {
+    console.log('toggleExpand...', option)
     let expand = this.state.expand ? false : true
     this.setState({
       expand
@@ -16,11 +21,11 @@ class FeedItem extends Component {
     return(
       <div onClick={this.toggleExpand}
         className={this.state.expand ? "feed-item expand" : "feed-item"}>
-
         <div className="feed-card">
           {this.props.name}
+          <Link to="" onClick={() => this.toggleExpand('like')}>赞</Link>
+          <Link to="" onClick={() => this.toggleExpand('comment')}>评论</Link>
         </div>
-
         <div className="feed-expand">
           {this.props.content}
         </div>
