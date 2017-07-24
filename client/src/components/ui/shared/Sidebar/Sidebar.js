15c71d1f015d89aa976daac015246dec1db5241a
Peter Wang <happypeter1983@gmail.com>
2017-07-24 15:27:20 +0800

    very neccery to disable user-scalable

--- client/src/components/ui/shared/Sidebar/Sidebar.js
+++ client/src/components/ui/shared/Sidebar/Sidebar.js
@@ -11,7 +11,7 @@ import {
 class Sidebar extends Component {
 
   state = {
-    isOpen: false
+    isOpen: true
   }
 
   closeMenu = () => {
@@ -29,18 +29,30 @@ class Sidebar extends Component {
   render () {
     const { currentUser, isAuthenticated } = this.props.account
     let userInfo = (
-      <div>
-        {currentUser}
+      <div className="user-info-text">
+        <div className="bm-user-name">
+          {currentUser}
+        </div>
         <Link to="" onClick={this.logout}>退出</Link>
       </div>
     )
     return (
       <Menu isOpen={this.state.isOpen}
             customCrossIcon={ false }>
-        {isAuthenticated ? userInfo : ''}
-        <Link onClick={this.closeMenu} to="/" className="menu-item" href="/">Home</Link>
-        <Link onClick={this.closeMenu} to="/signup" className="menu-item" href="/">注册</Link>
-        <button onClick={this.closeMenu} className ="bm-close-button" >关闭</button>
+            <div className="user-info">
+              <img className="bm-img"
+                src="http://media.haoduoshipin.com/yummy/default-avatar.png" alt="avatar" />
+              {isAuthenticated ? userInfo : ''}
+            </div>
+            <div className="bm-link-list">
+              <Link onClick={this.closeMenu} to="/" className="menu-item" >首页</Link>
+              <Link onClick={this.closeMenu} to="/profile" className="menu-item" >个人中心</Link>
+              <Link onClick={this.closeMenu} to="/cart" className="menu-item" >购物车</Link>
+              <Link onClick={this.closeMenu} to="/dishes" className="menu-item" >猜你喜欢</Link>
+            </div>
+            <div className="bottom-button">
+              <button onClick={this.closeMenu} className ="bm-close-button" >关闭</button>
+            </div>
       </Menu>
     );
   }
