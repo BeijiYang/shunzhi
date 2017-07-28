1c8a96de9b0e068c3fa827361749c34f8e8f09c5
Peter Wang <happypeter1983@gmail.com>
2017-07-28 23:07:20 +0800

    shop icon

--- client/src/components/ui/pages/Dish/Dish.js
+++ client/src/components/ui/pages/Dish/Dish.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
 import TitleHeader from '../../shared/TitleHeader/TitleHeader'
 import './dish.css'
+import ShoppingIcon from './ShoppingIcon'
+import { connect } from 'react-redux'
 
 class Dish extends Component {
+
+  state = {
+    buy: false
+  }
+
+  buy = () => {
+    console.log('buy...')
+    this.setState({
+      buy: true
+    })
+    this.props.dispatch({ type: 'ADD_CART', dishID: 'xxx'})
+  }
+
   render(){
     return(
       <div className="dish">
@@ -13,6 +28,16 @@ class Dish extends Component {
             </div>
           </div>
           <div className="dish-info-card">
+            <h1 className="dish-name">
+              草莓派
+            </h1>
+            <div className="price-tag">
+              24<span className="unit">元</span>
+            </div>
+            <div onClick={this.buy}
+              className="shopping-icon-wrap">
+              <ShoppingIcon color={this.state.buy ? '#F77062' : '#D0D0D0'}/>
+            </div>
           </div>
         </div>
       </div>
@@ -20,4 +45,4 @@ class Dish extends Component {
   }
 }
 
-export default Dish
+export default connect(null)(Dish)
