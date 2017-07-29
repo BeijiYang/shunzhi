7e48de74df76767e381d6e7147a1590e89ee309a
Peter Wang <happypeter1983@gmail.com>
2017-07-29 15:54:53 +0800

    item count

--- client/src/components/ui/pages/Cart/CartItem.js
+++ client/src/components/ui/pages/Cart/CartItem.js
@@ -3,15 +3,46 @@ import { connect } from 'react-redux'
 import './cart-item.css'
 
 class CartItem extends Component {
+
+  decrement(id){
+    console.log(id)
+  }
+
+  increment(id) {
+    console.log(id);
+  }
+
   render(){
-    const { name, poster } = this.props.dish
+    const { name, poster, price } = this.props.dish
     return(
       <div className="cart-item">
-        <div className="cart-item-poster"
-             style={{ 'backgroundImage': `url(${poster})`}}
-          >
+        <div className="cart-item-info">
+          <div className="cart-item-poster"
+               style={{ 'backgroundImage': `url(${poster})`}}
+            >
+          </div>
+          <div className="name-price-wrap">
+            <div className="cart-item-name">
+              {name}
+            </div>
+            <div className="cart-item-price">
+              {price}
+            </div>
+          </div>
         </div>
-        {name}
+        <div className="cart-action">
+          <div onClick={() => this.decrement('id')}
+            className="minus">
+            -
+          </div>
+          <div className="item-count">
+            2
+          </div>
+          <div onClick={() => this.increment('id')}
+            className="plus">
+            +
+          </div>
+        </div>
       </div>
     )
   }
