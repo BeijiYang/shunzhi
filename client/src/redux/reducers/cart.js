fd836f4768a5962c8c2ea4f5759655170e90eeac
Peter Wang <happypeter1983@gmail.com>
2017-07-29 22:52:26 +0800

    i

--- client/src/redux/reducers/cart.js
+++ client/src/redux/reducers/cart.js
@@ -1,6 +1,11 @@
 let cart = {
   total: 2,
   // dishes: ['597be20c2bbfdbaa14bfa248','597be2122bbfdbaa14bfa249'],
+  // dishes: {'597be20c2bbfdbaa14bfa248': {
+  //   name: '',
+  //   poster: '',
+  //   price: ''
+  // }},
   dishes: {},
   totalPrice: 0
 }
@@ -9,7 +14,14 @@ export default function cartReducer(state=cart, action) {
   switch (action.type) {
     case 'ADD_CART':
       console.log(action)
-      return { ...state, total: state.total + 1, dishes: [...state.dishes, action.dishId] }
+      let nextState = { ...state, total: state.total + 1, dishes: {...state.dishes, [action.dishId]: {
+        name: action.dish.name,
+        poster: action.dish.poster,
+        price: action.dish.price,
+        desc: action.dish.desc
+      }} }
+      console.log('====', nextState)
+      return nextState
     case 'INCR_CART_ITEM':
       // return { ...state, totalPrice: state.totalPrice + DISHES[action.dishId].price }
       return state
