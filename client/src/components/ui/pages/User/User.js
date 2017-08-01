c50f06c6c4880acc21bf0969de2d1edcd17c0350
Peter Wang <happypeter1983@gmail.com>
2017-08-01 16:03:26 +0800

    show user detail

--- client/src/components/ui/pages/User/User.js
+++ client/src/components/ui/pages/User/User.js
@@ -6,13 +6,28 @@ class User extends Component {
   render(){
     const { userId } = this.props.match.params
     const { users } = this.props
-    console.log('users', users)
-    return(
-      <div className="user">
-        <TitleHeader title="peter" />
-        { userId }
-      </div>
-    )
+    if(Object.keys(users).length !== 0){
+      console.log('users-inif', users)
+      const user = users[userId]
+      const { username, slogan, avatar } = user
+      const hisAvatar = avatar ? avatar : 'http://media.haoduoshipin.com/yummy/default-avatar.png'
+      const hisUsername = username ? username : 'no name'
+      const hisSlogan =  slogan ? slogan : '此人很个性，还没有填写个性签名'
+      return(
+        <div className="user">
+          <TitleHeader title="peter" />
+          <img src={hisAvatar} alt="avatar" />
+          <div className="user-name">
+            {hisUsername}
+          </div>
+          <div className="slogan">
+            {hisSlogan}
+          </div>
+        </div>
+      )
+    }else{
+      return null
+    }
   }
 }
 
