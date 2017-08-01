7b2b95e6ac4ffefa546fa1f843b504ca0832f131
Peter Wang <happypeter1983@gmail.com>
2017-08-01 14:55:31 +0800

    i

--- client/src/components/ui/pages/DashBoard/DashBoard.js
+++ client/src/components/ui/pages/DashBoard/DashBoard.js
@@ -2,6 +2,8 @@ import React , { Component } from 'react'
 import './dashboard.css'
 import FeedItem from  './FeedItem'
 import { connect } from 'react-redux'
+import TitleHeader from '../../shared/TitleHeader/TitleHeader'
+import { Link } from 'react-router-dom'
 
 class Dashboard extends Component {
 
@@ -13,10 +15,20 @@ class Dashboard extends Component {
         <FeedItem key={id} comment={comments[id]} />
       )
     })
+
+    const noUpdate = (
+      <div className="no-update">
+        暂无好友更新，可以直接去 <Link to="/dishes">购物区</Link> 转转。
+      </div>
+    )
+    console.log('dashboard', window.innerHeight)
     return(
       <div className="dashboard">
-        <div className="feed-wrap">
-          {Object.keys(comments).length === 0 ? '暂无好友更新' : cardList}
+        <TitleHeader title="好友更新" />
+        <div className="feed-wrap"
+             style={{ 'minHeight': `${window.innerHeight -80}px` }}
+          >
+          {Object.keys(comments).length === 0 ? noUpdate : cardList}
         </div>
       </div>
     )
