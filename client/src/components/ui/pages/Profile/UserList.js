97f7f4e566c1ddb6b6c214d7d2737303d9bd4fd0
Peter Wang <happypeter1983@gmail.com>
2017-08-01 17:57:46 +0800

    i

--- client/src/components/ui/pages/Profile/UserList.js
+++ client/src/components/ui/pages/Profile/UserList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './user-list.css'
+import Settings from '../../../../settings'
 
 class UserList extends Component {
   render(){
@@ -7,8 +8,14 @@ class UserList extends Component {
     const listStr = Object.keys(users).map(id => (
       <li className="user-list-item"
       key={id}>
-       <img src={users[id].avatar} alt="avatar" />
-       {users[id].username}
+       <img src={users[id].avatar ? users[id].avatar : Settings.defaultAvatar}
+         alt="avatar" />
+       <div className="user-list-username">
+         {users[id].username}
+       </div>
+       <div className="user-list-switch">
+         switch
+       </div>
     </li>
     ))
     console.log(listStr)
