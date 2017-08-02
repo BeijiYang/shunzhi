742ca5ae1d1319daafaa517ce6ad050b48ad7e17
Peter Wang <happypeter1983@gmail.com>
2017-08-02 17:04:57 +0800

    no password hash

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
-let SALT_WORK_FACTOR = 10;
+// var bcrypt = require('bcrypt');
+// let SALT_WORK_FACTOR = 10;
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var UserSchema = new mongoose.Schema(
@@ -14,27 +14,27 @@ var UserSchema = new mongoose.Schema(
 )
 // Schema(模式)的中间件方法，这里用不了了。。。因为在购物车里会有save方法的调用，会再次触发这个方法，导致密码出问题
 // e,这里还是不换了，将shopping中改为了更新方法
-UserSchema.pre('save',function (next) {
-  var user = this;
-
-  bcrypt.genSalt(SALT_WORK_FACTOR, function (err,salt) {
-    if (err) return next(err);
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      if (err) return next(err);
-      user.password = hash;
-      next()
-    })
-  })
-})
+// UserSchema.pre('save',function (next) {
+//   var user = this;
+//
+//   bcrypt.genSalt(SALT_WORK_FACTOR, function (err,salt) {
+//     if (err) return next(err);
+//     bcrypt.hash(user.password, salt, function (err, hash) {
+//       if (err) return next(err);
+//       user.password = hash;
+//       next()
+//     })
+//   })
+// })
 
 // user 的实例方法
 UserSchema.methods = {
   comparePassword: function (_password, cb) {
-    console.log(this.password);
-    bcrypt.compare(_password, this.password, function (err, isMatch) {
-      if (err) return cb(err);
+      let isMatch = false
+      if(this.password === _password) {
+        isMatch = true
+      }
       cb(null, isMatch);
-    })
   }
 }
 
