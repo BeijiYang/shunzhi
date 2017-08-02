fa965a129f1219ed3102f6ff343d53c96fb8a482
Peter Wang <happypeter1983@gmail.com>
2017-08-02 17:10:49 +0800

    cool

--- api/models/user.js
+++ api/models/user.js
@@ -33,6 +33,7 @@ UserSchema.methods = {
       let isMatch = false
       if(this.password === _password) {
         isMatch = true
+        console.log('isMatch is now: ', isMatch)
       }
       cb(null, isMatch);
   }
