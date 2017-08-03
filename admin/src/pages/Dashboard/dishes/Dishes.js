ac5b9fd2db49247e3b9c309d116054d4430dec6a
Peter Wang <happypeter1983@gmail.com>
2017-08-03 10:23:54 +0800

    i

--- admin/src/pages/Dashboard/dishes/Dishes.js
+++ admin/src/pages/Dashboard/dishes/Dishes.js
@@ -6,15 +6,27 @@ import { connect } from 'react-redux'
 
 class Dishes extends Component {
 
+  state = {
+    dishes: [
+      {
+        _id: '59819d9cf2b95493d2ee54ec',
+        name: '黑森林',
+        poster: 'http://xxx.png',
+        price: '23',
+        desc: '巧克力诱惑'
+      }
+    ]
+  }
+
   render() {
     console.log('Dishes', this.props.dishes)
-    if(Object.keys(this.props.dishes).length !== 0) {
+    if(Object.keys(this.state.dishes).length !== 0) {
       return (
         <div className='page'>
           <div className='white-block'>
-            <div>共{Object.keys(this.props.dishes).length}条</div>
+            <div>共{Object.keys(this.state.dishes).length}条</div>
             <Table columns={OrderTableColumns}
-              dataSource={this.props.dishes}
+              dataSource={this.state.dishes}
               pagination={{
                 total: Object.keys(this.props.dishes).length,
                 defaultPageSize: 10
