fc1155528bc51ac9b876a371e82b22f5a72eeb69
Peter Wang <happypeter1983@gmail.com>
2017-08-03 10:14:09 +0800

    dish table

--- admin/src/pages/Dashboard/dishes/Dishes.js
+++ admin/src/pages/Dashboard/dishes/Dishes.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
+import { Table } from 'antd'
+import OrderTableColumns from './DishTableColumns'
+
 
 class Dishes extends Component {
-  render(){
-    return(
-      <div className="dishes">
-        Dishes
+  state = {
+    shops: [
+      {
+        name: '黑森林',
+        username: 'happypeter',
+        _id: 'xxxxxx'
+      },
+      {
+        name: '提拉米苏',
+        username: 'billie',
+        _id: 'x44xxxx'
+      }
+    ],
+    total: 4,
+  }
+
+  render() {
+    return (
+      <div className='page'>
+        <div className='white-block'>
+          <div>共{Object.keys(this.state.shops).length}条订单</div>
+          <Table columns={OrderTableColumns}
+            dataSource={this.state.shops}
+            pagination={{
+              total: this.state.total,
+              defaultPageSize: 10
+            }}
+            rowKey={record => record._id}
+          />
+        </div>
       </div>
     )
   }
