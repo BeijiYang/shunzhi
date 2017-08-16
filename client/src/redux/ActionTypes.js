8d5a504b0846c2e8e7c2d58320a3408e909d582a
Peter Wang <happypeter1983@gmail.com>
2017-08-16 16:25:12 +0800

    i

--- client/src/redux/ActionTypes.js
+++ client/src/redux/ActionTypes.js
@@ -1,2 +1,3 @@
 export const LOAD_CURRENT_USER = 'LOAD_CURRENT_USER'
 export const UPDATE_USER       = 'UPDATE_USER'
+export const LOAD_USERS        = 'LOAD_USERS'
--- client/src/redux/reducers/user.js
+++ client/src/redux/reducers/user.js
@@ -5,12 +5,8 @@ let user = {
 
 export default function userReducer(state=user, action) {
   switch (action.type) {
-    case 'LOAD_USERS':
+    case types.LOAD_USERS:
       return { ...state, all: action.users }
-    case types.UPDATE_USER:
-      // FIXME
-      let nextState = state
-      return nextState
     default:
       return state
   }
