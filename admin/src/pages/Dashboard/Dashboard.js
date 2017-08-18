d353b7f92ba2c71bd243f6025d3b7f88061d9848
Peter Wang <happypeter1983@gmail.com>
2017-08-19 07:34:55 +0800

    i

--- admin/src/pages/Dashboard/Dashboard.js
+++ admin/src/pages/Dashboard/Dashboard.js
@@ -3,10 +3,7 @@ import Sidebar from './sidebar/Sidebar'
 import { Route, Switch } from 'react-router-dom'
 import './dashboard.css'
 import NewDish from './dishes/NewDish'
-
-
 import Dishes from './dishes/Dishes'
-
 import Orders from './orders/Orders'
 
 const Dashboard = ({match, history}) => (
@@ -16,10 +13,11 @@ const Dashboard = ({match, history}) => (
       <div className='top-header'></div>
       <div className='content'>
         <Switch>
+          <Route exact path={`${match.url}/`} component={Dishes} />
           <Route exact path={`${match.url}/dishes`} component={Dishes} />
           <Route exact path={`${match.url}/orders`} component={Orders} />
           <Route exact path={`${match.url}/dishes/new`} component={NewDish} />
-          <Route render={ () => <div>TODO</div> } />
+          <Route render={ () => <div>对不起，404 了</div> } />
         </Switch>
       </div>
     </div>
--- admin/src/pages/Dashboard/dishes/DishTableColumns.js
+++ admin/src/pages/Dashboard/dishes/DishTableColumns.js
@@ -3,35 +3,40 @@ import { Link } from 'react-router-dom'
 import Settings from '../../../settings'
 
 const DishTableColumns = [
-{
-  title: '海报',
-  dataIndex: 'poster',
-  key: 'poster',
-  render: (text) => {
-    return <img className="poster" src={Settings.host + '/uploads/posters/' + text} />
+  {
+    title: '海报',
+    dataIndex: 'poster',
+    key: 'poster',
+    render: (text) => {
+      return <img className="poster" src={Settings.host + '/uploads/posters/' + text} />
+    },
   },
-},{
-  title: '菜品名称',
-  dataIndex: 'name',
-  key: 'name',
-}, {
-  title: '描述',
-  dataIndex: 'desc',
-  key: 'desc',
-}, {
-  title: '价格',
-  dataIndex: 'price',
-  key: 'price',
-  render: (text) => {
-    return <span>{`${text}元`}</span>
+  {
+    title: '菜品名称',
+    dataIndex: 'name',
+    key: 'name',
   },
-}, {
-  title: '操作',
-  dataIndex: '_id',
-  key: '_id',
-  render: (text) => {
-    return <Link to={`/dashboard/dishes/${text}`}>修改</Link>
+  {
+    title: '描述',
+    dataIndex: 'desc',
+    key: 'desc',
   },
-}]
+  {
+    title: '价格',
+    dataIndex: 'price',
+    key: 'price',
+    render: (text) => {
+      return <span>{`${text}元`}</span>
+    },
+  },
+  {
+    title: '操作',
+    dataIndex: '_id',
+    key: '_id',
+    render: (text) => {
+      return <Link to={`/dashboard/dishes/${text}`}>修改</Link>
+    },
+  }
+]
 
 export default DishTableColumns
