c7874e2faec9fe4f419438d033df3695dd05362d
Peter Wang <happypeter1983@gmail.com>
2017-08-19 16:45:20 +0800

    rm DishTableColumns

--- admin/src/components/Dishes.js
+++ admin/src/components/Dishes.js
@@ -1,6 +1,50 @@
 import React from 'react'
 import { Table } from 'antd'
-import DishTableColumns from './DishTableColumns'
+import { Link } from 'react-router-dom'
+import Settings from '../settings'
+import styled from 'styled-components'
+
+const Poster = styled.img`
+	display: block;
+	width: 100px;
+`
+
+const DishTableColumns = [
+  {
+    title: '海报',
+    dataIndex: 'poster',
+    key: 'poster',
+    render: (text) => {
+      return <Poster src={Settings.host + '/uploads/posters/' + text} />
+    },
+  },
+  {
+    title: '菜品名称',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '描述',
+    dataIndex: 'desc',
+    key: 'desc',
+  },
+  {
+    title: '价格',
+    dataIndex: 'price',
+    key: 'price',
+    render: (text) => {
+      return <span>{`${text}元`}</span>
+    },
+  },
+  {
+    title: '操作',
+    dataIndex: '_id',
+    key: '_id',
+    render: (text) => {
+      return <Link to="" onClick={() => console.log('delete')}>删除</Link>
+    },
+  }
+]
 
 const Dishes = ({ dishes }) => (
   <div className='page'>
